435ec20345beb79b5820aea3f70869ed69f04c9f
WIN_NT\x16j025 <x16j025@chiba-fjb.ac.jp>
2017-05-17 12:32:14 +0900

    Exa

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Example10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnJudgr = new System.Windows.Forms.Button();
+            this.tbxAge = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "年齢は";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "歳";
+            // 
+            // btnJudgr
+            // 
+            this.btnJudgr.Location = new System.Drawing.Point(212, 57);
+            this.btnJudgr.Name = "btnJudgr";
+            this.btnJudgr.Size = new System.Drawing.Size(60, 19);
+            this.btnJudgr.TabIndex = 4;
+            this.btnJudgr.Text = "判定";
+            this.btnJudgr.UseVisualStyleBackColor = true;
+            this.btnJudgr.Click += new System.EventHandler(this.btnJudgr_Click);
+            // 
+            // tbxAge
+            // 
+            this.tbxAge.Location = new System.Drawing.Point(67, 57);
+            this.tbxAge.Name = "tbxAge";
+            this.tbxAge.Size = new System.Drawing.Size(73, 19);
+            this.tbxAge.TabIndex = 5;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(119, 123);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbxAge);
+            this.Controls.Add(this.btnJudgr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "年齢判定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnJudgr;
+        private System.Windows.Forms.TextBox tbxAge;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
